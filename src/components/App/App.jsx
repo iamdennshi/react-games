9b984e91aa43692bfe03f8f8545d7bdc0d3d6b88
Den <iamdennshi@gmail.com>
2023-08-05 03:25:43 +0500

    Add slider and first poster

---
+++ src/components/App/App.jsx
@@ -0,0 +1,15 @@
+import Header from '../header/Header'
+import Slider from '../slider/Slider'
+
+function App() {
+  return (
+    <div className="App">
+      <Header />
+      <main className='main'>
+        <Slider />
+      </main>
+    </div>
+  );
+}
+
+export default App;
--- src/components/slider/Slider.jsx
+++ src/components/slider/Slider.jsx
@@ -1,20 +1,41 @@
 import "./style.scss";
+import { useState } from "react";
 
 function Slider() {
-  return (
-    <section className="slider">
-      <button className="slider__button-prev">prev</button>
-      <button className="slider__button-next">next</button>
-      <ul className="slider__items">
-        <li className="slider__item">
-          Hello
-        </li>
-        <li className="slider__item">
-          World
-        </li>
-      </ul>
 
-    </section>
+  const numberOfPosters = 5;
+  const shift = 100.0 / numberOfPosters;
+  const [posterIndex, setPosterIndex] = useState(0);
+  const handleClick = (event, type) => {
+    if (type) {
+      setPosterIndex(() => (posterIndex + 1) % numberOfPosters);
+    } else {
+      setPosterIndex(() => (posterIndex - 1 >= 0) ? posterIndex - 1 : numberOfPosters - 1); 
+    }
+  }
+
+  return (
+    <div className="carousel">
+      <ul style={{width: `${numberOfPosters * 100}%`, transform: `translate(-${posterIndex * shift}%)`}} className="carousel__slider">
+        <li className="test">
+          <img src="/img/games/hogwarts.png" alt="" className="test__img" />
+          <h3 className="test__title">Получите <span className="test__sale">скидку 50%</span> на первую игру</h3>
+          <button className="test__button">Купить</button>
+          <img className="test__poster" src="/img/sliders/hogwarts.png" alt="" />
+        </li>
+        <li style={{backgroundColor: 'blue'}}>Second</li>
+        <li style={{backgroundColor: 'yellow'}}>Third</li>
+        <li style={{backgroundColor: 'green'}}>Fourth</li>
+        <li style={{backgroundColor: 'pink'}}>Fifth</li>
+      </ul>
+      <div className="carousel__controls">
+        <span onClick={(e) => handleClick(e, 0)} className="carousel__button carousel__button--left">
+           <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 8 8"><path d="M5.25 0l-4 4 4 4 1.5-1.5-2.5-2.5 2.5-2.5-1.5-1.5z"/></svg></span>
+        <span onClick={(e) => handleClick(e, 1)} className="carousel__button carousel__button--right">
+          <svg xmlns="http://www.w3.org/2000/svg" fill="#fff" viewBox="0 0 8 8"><path d="M2.75 0l-1.5 1.5 2.5 2.5-2.5 2.5 1.5 1.5 4-4-4-4z"/></svg>
+        </span>
+      </div>
+    </div>
   );
 }
 
