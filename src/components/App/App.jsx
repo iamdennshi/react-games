2fc0ead38498e7009fcbd0cbae538231bca624f7
Den <iamdennshi@gmail.com>
2023-08-09 12:37:12 +0500

    Add cart component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,6 +1,8 @@
 import Header from '../header/Header';
 import Slider from '../slider/Slider';
 import Games from '../games/Games';
+import Cart from '../cart/Cart';
+
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <main className='main'>
         <Slider />
         <Games />
+        <Cart />
       </main>
     </div>
   );
---
+++ src/components/cart/Cart.jsx
@@ -0,0 +1,43 @@
+import "./styles.scss";
+
+
+function Cart() {
+    return (
+        <div className="overlay">
+            <div className="cart">
+                <h2 className="cart__title">Корзина</h2>
+                <div className="item">
+                    <div className="item__logo"><img className="item__img" src="/img/games/atomic.png" alt="" /></div>
+                    <div className="item__info">
+                        <h3 className="item__title">Atomic Heart</h3>
+                        <span className="item__price">3 999 руб.</span>
+                    </div>
+                    <button className="item__delete-button">
+                        <svg width="10" height="10" viewBox="0 0 10 10" fill="none" xmlns="http://www.w3.org/2000/svg">
+                            <path d="M7.61539 1.25353L5.16658 3.70234L2.71786 1.25362C1.77362 0.309374 0.309197 1.77379 1.25344 2.71804L3.70216 5.16676L1.25336 7.61556C0.309321 8.5596 1.77369 10.024 2.71773 9.07993L5.16653 6.63113L7.61525 9.07985C8.55949 10.0241 10.0239 8.55967 9.07967 7.61543L6.63095 5.16671L9.07976 2.7179C10.0239 1.77375 8.55954 0.309376 7.61539 1.25353Z" fill="#D3D3D3" />
+                        </svg>
+
+                    </button>
+                </div>
+                <div className="cart__footer">
+                    <div className="cart__total">
+                        <p className="cart__total-text">Итого:</p>
+                        <div className="cart__line"></div>
+                        <span className="cart__total-value">7 998 руб.</span>
+                    </div>
+                    <div className="cart__tax">
+                        <p className="cart__tax-text">Налог 5%:</p>
+                        <div className="cart__line"></div>
+                        <span className="cart__tax-value">399 руб.</span>
+                    </div>
+                    <button className="cart__button-order button-move">
+                        <p className="button-move__text">Оформить заказ</p>
+                        <img src="/img/icons/arrow-to-right.svg" alt="" />
+                    </button>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Cart;
